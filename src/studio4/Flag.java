2988067e18c79882671d0af0aef949755e95f686
gavin <gavin@localhost>
2023-02-23 15:21:23 -0600

    ilghjiuhoh

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,13 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.setPenColor(Color.RED);
+		StdDraw.setPenRadius(0.5);
+		StdDraw.filledRectangle(0.9, 0.9, 1,0.3);
+		StdDraw.filledRectangle(0.4, 0.1, 1,0.3);
+		//StdDraw.filledRectangle(0.3, 0.4, 0.1,1);
+		
+		StdDraw.show();
 		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,6 +1,6 @@
 package studio4;
 
-import java.io.File;
+import java.io.File;	
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
@@ -8,6 +8,7 @@ import javax.swing.JFileChooser;
 import javax.swing.SwingUtilities;
 
 import edu.princeton.cs.introcs.StdDraw;
+import java.awt.Color; 
 
 /**
  * @author Dennis Cosgrove (http://www.cse.wustl.edu/~cosgroved/)
@@ -19,6 +20,26 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		//ellipse 73 0 146 false 0.5 0.5 0.2 0.2
+		String shape = in.next();
+		int redComponent = in.nextInt();
+		int greenComponent = in.nextInt();
+		int blueComponent = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		double paramater1 = in.nextDouble();
+		double paramater2 = in.nextDouble();
+		double paramater3 = in.nextDouble();
+		double paramater4 = in.nextDouble();
+		Color Movember = new Color(redComponent, greenComponent, blueComponent);
+		StdDraw.setPenColor(Movember);
+		
+		if (shape.equals("rectangle")){
+			if(isFilled == true)
+			StdDraw.filledRectangle(paramater1, paramater2, paramater2, paramater2);
+			
+		}
+			
+		
 		
 	}
 }
